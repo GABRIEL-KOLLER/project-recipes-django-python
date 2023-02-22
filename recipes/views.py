6d5a11e55c87f4589be2a80cf678e813188c8b4f
Gabriel S. Koller <95451205+GABRIEL-KOLLER@users.noreply.github.com>
2023-02-22 19:46:40 -0300

    Add files via upload

---
+++ recipes/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from utils.recipes.factory import make_recipe
+
+from .models import Recipe
+
+
+def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+    return render(request,'recipes/pages/home.html', context={
+        'recipes': recipes,
+    } )
+
+def category(request, category_id):
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+        ).order_by('-id')
+    )
+
+
+    return render(request,'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |'
+    } )
+
+
+def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
+    return render(request,'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    } )
